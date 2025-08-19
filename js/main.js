7e35b7bbd11d9b00f87fbab398d4540a7ebe8a07
josue321 <josueyarleque@upeu.edu.pe>
2025-08-19 18:08:49 -0500

    Actu19/08

--- js/main.js
+++ js/main.js
@@ -1,16 +1,6 @@
 (function ($) {
   "use strict";
 
-  // Spinner
-  var spinner = function () {
-    setTimeout(function () {
-      if ($("#spinner").length > 0) {
-        $("#spinner").removeClass("show");
-      }
-    }, 1);
-  };
-  spinner();
-
   // Initiate the wowjs
   new WOW().init();
 
@@ -27,30 +17,6 @@
   $('[data-toggle="counter-up"]').counterUp({
     delay: 10,
     time: 2000,
-  });
-
-  // Back to top button
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 100) {
-      $(".back-to-top").fadeIn("slow");
-    } else {
-      $(".back-to-top").fadeOut("slow");
-    }
-  });
-  $(".back-to-top").click(function () {
-    $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
-    return false;
-  });
-
-  // Testimonials carousel
-  $(".testimonial-carousel").owlCarousel({
-    items: 1,
-    autoplay: true,
-    smartSpeed: 1000,
-    dots: true,
-    loop: true,
-    nav: true,
-    navText: ['<i class="bi bi-chevron-left"></i>', '<i class="bi bi-chevron-right"></i>'],
   });
 })(jQuery);
 
